01a860f5f781dc9a955b5dcb06db6286597f73c6
jim <jim@formatc.net>
2016-01-06 15:39:00 +0000

    Minor update to footer and header, add underscore.js

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,3 @@
 
-    <script src="/assets/js/jquery-1.11.3.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/assets/js/bootstrap.min.js"></script>
   </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -7,8 +7,8 @@
     <title>SkyBet Tech Test</title>
 
     <!-- Bootstrap -->
-    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/css/bootstrap-cerulean.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/css/bootstrap-cerulean.min.css'); ?>" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,6 +16,11 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
+    
+    <script src="<?php echo base_url('assets/js/jquery-1.11.3.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/underscore-min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
   </head>
   
   <body>
@@ -30,12 +35,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Brand</a>
+          <a class="navbar-brand" href="#">TechTest</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="<?php echo base_url('/'); ?>">List People</a></li>
+            <li><a href="<?php echo base_url('/'); ?>">List People</a></li>
             <li><a href="/tests">Run Tests</a></li>
           </ul>
         </div><!-- /.navbar-collapse -->
