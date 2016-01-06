06816c9dd82f0a44b5e7a7b1c9ed303256c5d8c7
jim <jim@formatc.net>
2016-01-06 13:43:35 +0000

    Add datahandler model, update people controller to load / save data, add header & footer views, update people view to itterate over loaded data

--- application/controllers/people.php
+++ application/controllers/people.php
@@ -3,8 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class People extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->view('people/index');
+	public function index() {
+    $this->load->model('Data_handler');
+    
+    $dh = new Data_handler();
+    $data['people'] = $dh->load();
+    
+		$this->load->view('header');
+		$this->load->view('people/index', $data);
+    $this->load->view('footer');
 	}
+    
+  public function save() {
+    $this->load->model('Data_handler');
+    
+    $people = $this->input->post('people');
+    
+    $dh = new Data_handler();
+    $dh->save($people);
+    
+    redirect(base_url('people'));
+  }
+  
 }
--- application/views/people/index.php
+++ application/views/people/index.php
@@ -1,29 +1,23 @@
-<form>
-    <table>
-        <tr>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      
+      <form action="<?php echo base_url('/people/save'); ?>" method="post">
+        <table>
+          <tr>
             <th>First name</th>
             <th>Last name</th>
-        </tr>
-        <tr>
-            <td><input type="text" name="people[][firstname]" value="Leonard" /></td>
-            <td><input type="text" name="people[][surname]" value="Hofstader" /></td>
-        </tr>
-        <tr>
-            <td><input type="text" name="people[][firstname]" value="Sheldon" /></td>
-            <td><input type="text" name="people[][surname]" value="Cooper" /></td>
-        </tr>
-        <tr>
-            <td><input type="text" name="people[][firstname]" value="Raj" /></td>
-            <td><input type="text" name="people[][surname]" value="Koothrapali" /></td>
-        </tr>
-        <tr>
-            <td><input type="text" name="people[][firstname]" value="Howard" /></td>
-            <td><input type="text" name="people[][surname]" value="Wolowitz" /></td>
-        </tr>
-        <tr>
-            <td><input type="text" name="people[][firstname]" value="Penny" /></td>
-            <td><input type="text" name="people[][surname]" value="" /></td>
-        </tr>
-    </table>
-    <input type="submit" value="OK" />
-</form>
+          </tr>
+          <?php for($i = 0; $i < count($people); $i++) : ?>
+          <tr>
+            <td><input type="text" name="people[<?php echo $i; ?>][firstname]" value="<?php echo $people[$i]['firstname']; ?>" /></td>
+            <td><input type="text" name="people[<?php echo $i; ?>][surname]" value="<?php echo $people[$i]['surname']; ?>" /></td>
+          </tr>
+          <?php endfor; ?>
+        </table>
+        <input type="submit" value="OK" />
+      </form>
+      
+    </div><!-- end col -->
+  </div><!-- end row -->
+</div><!-- end container -->
