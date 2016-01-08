a95eb523f3309e4077abe858615720a86162b4f2
jim <jim@formatc.net>
2016-01-08 08:59:44 +0000

    Add home page

---
+++ application/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <h1>Sky Tech Test</h1>
+        <ul>
+          <li><a href="<?php echo site_url('/people'); ?>">List People</a></li>
+          <li><a href="<?php echo site_url('/tests'); ?>">Run Tests</a></li>
+        </ul>
+    </div><!-- end col -->
+  </div><!-- end row -->
+</div><!-- end container -->
